0bd77f6cc6aa429aa8d3617acb246fbf38ccb8cc
Migori <92369545+migoriA@users.noreply.github.com>
2022-07-27 16:36:29 +0200

    Es3

---
+++ Es3/Es3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserire un'ora con il , da separatore: ");
+            decimal ora = Convert.ToDecimal(Console.ReadLine());
+            string[] sep = new string[2];
+            sep = ora.ToString().Split(',');
+
+            int ore = Convert.ToInt32(sep[0]);
+            int minuti= Convert.ToInt32(sep[1]);
+            int secondi = (ore * 3600) + (minuti * 60);
+            Console.Write($"\nCi sono {secondi} in {ora}");
+            Console.ReadLine();
+        }
+    }
+}
