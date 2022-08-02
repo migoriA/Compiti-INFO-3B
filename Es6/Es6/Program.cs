cc7f2fab58b08680f577ccdfa36f242b78415a1a
Migori <92369545+migoriA@users.noreply.github.com>
2022-08-02 15:30:08 +0200

    Es6

---
+++ Es6/Es6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dado=0;
+            Random rnd = new Random();
+
+            do
+            {
+                dado = rnd.Next(0, 7);
+                if (dado != 0)
+                {
+                    Console.WriteLine(dado);
+                }
+            } while (dado!=0);
+            
+            Console.ReadKey();
+        }
+    }
+}
