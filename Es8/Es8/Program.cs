1d5a87ec345e848d2566f4a9808de40881472463
Migori <92369545+migoriA@users.noreply.github.com>
2022-08-02 15:51:46 +0200

    Es8

---
+++ Es8/Es8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            char car=' ';
+            int cont = 0;
+            Console.Write("Inserire una frase: ");
+            str = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Inserire un carattere: ");
+            car = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine();
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == car)
+                {
+                    cont++;
+                }
+            }
+
+            Console.WriteLine($"Il carattere e' presente per {cont} volte");
+            Console.ReadKey();
+        }
+    }
+}
